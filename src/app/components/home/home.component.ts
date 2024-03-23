048329bfd0851ef73f206e9fe67363e781f8f39d
mervs <mervsen@gmail.com>
2024-03-23 14:10:24 +0300

    initial

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { InputNumberModule } from 'primeng/inputnumber';
+interface DoktorType {
+  name: string;
+  checked: boolean;
+  numberForOneDay: number;
+}
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ButtonModule, ToggleButtonModule, FormsModule, InputNumberModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  checked: boolean = false;
+  doctorTypes : DoktorType[] =[
+    {name:'Şef', checked: false, numberForOneDay:1},
+    {name:'Uzman Doktor', checked: false, numberForOneDay:1},
+    {name:'Kıdemli Asistan', checked: false, numberForOneDay:1},
+    {name:'Orta Kıdem Asistan', checked: false, numberForOneDay:1},
+    {name:'Çömez Asistan', checked: false, numberForOneDay:1},
+    {name:'Intern', checked: false, numberForOneDay:1},
+    {name:'Diğer', checked: false, numberForOneDay:1},
+  ]
+}

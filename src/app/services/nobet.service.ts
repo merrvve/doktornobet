0e0dda3dd5860554128e903ff288880274eddc3c
mervs <mervsen@gmail.com>
2024-03-23 23:53:36 +0300

    algorithm not possible days

--- src/app/services/nobet.service.ts
+++ src/app/services/nobet.service.ts
@@ -8,7 +8,8 @@ interface Day {
 
 interface Person {
   personId: number;
-  nobetDays: number[];
+  shiftDays: number[];
+  holidayShiftDays: number[];
   notPossible: number[];
   desiredDays: number[];
 }
@@ -22,16 +23,20 @@ interface Person {
 export class NobetService {
 
   days: Day[] = [];
+  holidays: number[]=[];
   persons: Person[] =[];
   finishedPersons: number[]=[];
+  finishedPersonsForHolidays: number[]=[];
+
   constructor() {
     this.persons = [
-      {personId:1,nobetDays:[],notPossible:[],desiredDays:[]},
-      {personId:2,nobetDays:[],notPossible:[],desiredDays:[]},
-      {personId:3,nobetDays:[],notPossible:[],desiredDays:[]},
-      {personId:4,nobetDays:[],notPossible:[],desiredDays:[]}
+      {personId:1,shiftDays:[],notPossible:[2,3,4,5],desiredDays:[],holidayShiftDays:[]},
+      {personId:2,shiftDays:[],notPossible:[],desiredDays:[],holidayShiftDays:[]},
+      {personId:3,shiftDays:[],notPossible:[2,7,8,9,10],desiredDays:[],holidayShiftDays:[]},
+      {personId:4,shiftDays:[],notPossible:[2,3,4],desiredDays:[],holidayShiftDays:[]}
     ];
     const holidays= [6,7,13,14,20,21,27,28]
+    this.holidays=holidays;
     let isHoliday=false;
     for (let i=1; i<31; i++) {
       isHoliday=false;
@@ -48,23 +53,62 @@ export class NobetService {
     
    }
 
-
-   createShifts(days: Day[]=this.days,persons: Person[]=this.persons){
-    const shiftPerPerson = Number(days.length/persons.length)
-    console.log(shiftPerPerson)
-    for (let i=0; i<days.length; i++){
-      days[i]=this.deleteFinished(days[i]);
-      const selectedId= this.sample(days[i].possiblePersonIds);
-      days[i].workingPersonId = selectedId;
-      const selectedPerson = this.persons.find(x=>x.personId==selectedId)
-      if(selectedPerson) {
-        selectedPerson.nobetDays.push(i);
-        if(selectedPerson.nobetDays.length>=shiftPerPerson) {
-          this.finishedPersons.push(selectedId);
+   markNotPossibleDays(persons: Person[]=this.persons, days: Day[]=this.days) {
+    for(const person of persons) {
+      for (const busyDay of person.notPossible) {
+        const index = days[busyDay-1].possiblePersonIds.indexOf(person.personId);
+        if(index>-1) {
+          days[busyDay-1].possiblePersonIds.splice(index,1);
         }
       }
-      if(i+1<days.length) {
-        days[i+1].possiblePersonIds.splice(days[i+1].possiblePersonIds.indexOf(selectedId),1)
+    }
+   }
+
+   
+
+
+   createShifts(days: Day[]=this.days,persons: Person[]=this.persons, holidays: number[]=this.holidays){
+    this.markNotPossibleDays(persons,days);
+    const shiftPerPerson = Number((days.length-holidays.length)/persons.length)
+    const holidayShiftPerPerson = Number(holidays.length/persons.length)
+    console.log(shiftPerPerson)
+    for (let i=0; i<days.length; i++){
+      if (days[i].workingPersonId==0) {
+        if(days[i].isHoliday) {
+          days[i]=this.deleteFinished(days[i],this.finishedPersonsForHolidays);
+        }
+        else {
+          days[i]=this.deleteFinished(days[i],this.finishedPersons);      
+        }
+        const selectedId= this.sample(days[i].possiblePersonIds);
+        if(selectedId>-1) {
+          days[i].workingPersonId = selectedId;
+          const selectedPerson = this.persons.find(x=>x.personId==selectedId)
+          if(selectedPerson) {
+            if(days[i].isHoliday) {
+              selectedPerson.holidayShiftDays.push(i);
+              if(selectedPerson.holidayShiftDays.length>=holidayShiftPerPerson) {
+                this.finishedPersonsForHolidays.push(selectedId);
+              }
+            }
+            else {
+              selectedPerson.shiftDays.push(i);
+              if(selectedPerson.shiftDays.length>=shiftPerPerson) {
+                this.finishedPersons.push(selectedId);
+              }
+            }
+            
+          }
+          if(i+1<days.length) {
+            days[i+1].possiblePersonIds.splice(days[i+1].possiblePersonIds.indexOf(selectedId),1)
+          }
+        }
+        else {
+          console.log('No possible person for day: ',i+1);
+          return;
+        }
+        
+  
       }
     }
     console.log(days);
@@ -73,14 +117,20 @@ export class NobetService {
     console.log(days.filter(x=>x.workingPersonId==3))
     console.log(days.filter(x=>x.workingPersonId==4))
   
+    console.log(persons)
    }
 
    sample(array: number[]){
-    return array[Math.floor(Math.random()*array.length)];
+    if(array.length>0) {
+      return array[Math.floor(Math.random()*array.length)];
+    }
+    else {
+      return -1;
+    }
   }
 
-  deleteFinished(day:Day) {
-    for (const item of this.finishedPersons) {
+  deleteFinished(day:Day,finishedList: number[]) {
+    for (const item of finishedList) {
       const index = day.possiblePersonIds.indexOf(item);
       if(index>-1) {
         day.possiblePersonIds.splice(index,1);

e9ced999397f89303973f5f3f5da03644a258884
mervs <mervsen@gmail.com>
2024-03-30 02:16:22 +0300

    ui

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,10 +4,14 @@ import { ButtonModule } from 'primeng/button';
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { NobetService } from '../../services/nobet.service';
+import { StepperModule } from 'primeng/stepper';
+import { CalendarModule } from 'primeng/calendar';
+
 interface DoktorType {
   name: string;
   checked: boolean;
   numberForOneDay: number;
+  total: number;
 }
 interface Day {
   dayNo: number;
@@ -20,23 +24,23 @@ interface Day {
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, ToggleButtonModule, FormsModule, InputNumberModule],
+  imports: [ButtonModule, ToggleButtonModule, FormsModule, InputNumberModule, StepperModule, CalendarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  date: Date[] | undefined;
 
   days : Day[] =[];
   checked: boolean = false;
   doctorTypes : DoktorType[] =[
-    {name:'Şef', checked: false, numberForOneDay:1},
-    {name:'Uzman Doktor', checked: false, numberForOneDay:1},
-    {name:'Kıdemli Asistan', checked: false, numberForOneDay:1},
-    {name:'Orta Kıdem Asistan', checked: false, numberForOneDay:1},
-    {name:'Çömez Asistan', checked: false, numberForOneDay:1},
-    {name:'Intern', checked: false, numberForOneDay:1},
-    {name:'Diğer', checked: false, numberForOneDay:1},
+    {name:'Şef', checked: false, numberForOneDay:1, total: 5},
+    {name:'Uzman Doktor', checked: false, numberForOneDay:1, total: 5},
+    {name:'Kıdemli Asistan', checked: false, numberForOneDay:1, total: 5},
+    {name:'Orta Kıdem Asistan', checked: false, numberForOneDay:1, total: 5},
+    {name:'Çömez Asistan', checked: false, numberForOneDay:1, total: 5},
+    {name:'Intern', checked: false, numberForOneDay:1, total: 5},
+    {name:'Diğer', checked: false, numberForOneDay:1, total: 5},
   ]
 
 
